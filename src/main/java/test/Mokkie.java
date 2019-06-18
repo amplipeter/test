5eaa57349efd25c2de40640e89c25ba5b091c7d7
Peter Abma <charlatans@chello.nl>
2019-06-18 22:42:54 +0200

    test

--- src/main/java/test/Mokkie.java
+++ src/main/java/test/Mokkie.java
@@ -4,7 +4,7 @@ public class Mokkie {
 
 	public static void main(String[] args) {
 		Showme s = new Showme();
-		String returnString = s.showPrintln("abcdefghijk");
+		String returnString = s.showPrintln("ab");
 		System.out.println(returnString);
 		
 	}
